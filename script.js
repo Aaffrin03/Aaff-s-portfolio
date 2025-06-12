43d27c16c2c12c3fffabbbe1d1242b48dda279da
Aaffrin03 <aaffrin2006@gmail.com>
2025-06-12 22:07:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const roles = [
+  "software developer",
+  "web developer",
+  "UI/UX designer",
+  "problem solver",
+  "critical thinker",
+  "tech enthusiast"
+];
+
+let index = 0;
+let charIndex = 0;
+let currentRole = "";
+const typeElement = document.getElementById("typewriter");
+
+function type() {
+  if (charIndex < roles[index].length) {
+    currentRole += roles[index].charAt(charIndex);
+    typeElement.textContent = currentRole;
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    currentRole = currentRole.substring(0, charIndex - 1);
+    typeElement.textContent = currentRole;
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    index = (index + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  type();
+});
